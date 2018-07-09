d91981864c3fa8416b2b9193d589eb5bd64587c4
rayfrancojr <31894035+rayfrancojr@users.noreply.github.com>
2018-07-08 20:14:47 -0500

    Changed the print statement to a flipCardMethod

--- Concentration/ViewController.swift
+++ Concentration/ViewController.swift
@@ -11,9 +11,18 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBAction func touchCard(_ sender: UIButton) {
-        print("agh! a ghost!")
-        print("Hello CS163p")
+       flipCard(withEmoji: "👻", on: sender)
     }
     
+    func flipCard(withEmoji Emoji: String, on button: UIButton) {
+        if button.currentTitle == "👻" {
+          button.setTitle("", for: UIControlState.normal)
+            button.backgroundColor = #colorLiteral(red: 1, green: 0.5763723254, blue: 0, alpha: 1)
+        }
+        else {
+            button.setTitle("👻", for: UIControlState.normal)
+            button.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+        }
+        }
 }
 
