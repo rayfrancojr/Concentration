024fa44667b2c8a255ec842206bab7a94dea34eb
rayfrancojr <31894035+rayfrancojr@users.noreply.github.com>
2018-07-30 18:25:15 -0500

    End of Session 1

--- Concentration/ViewController.swift
+++ Concentration/ViewController.swift
@@ -9,20 +9,39 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    @IBAction func touchCard(_ sender: UIButton) {
-       flipCard(withEmoji: "👻", on: sender)
+    
+    var flipCount = 0 {
+        didSet {
+            flipCountLabel.text = "Flips = \(flipCount)"
+        }
     }
     
-    func flipCard(withEmoji Emoji: String, on button: UIButton) {
-        if button.currentTitle == "👻" {
-          button.setTitle("", for: UIControlState.normal)
+    var emojiChoices  = ["🎃","👻","🎃","👻"]
+    @IBOutlet weak var flipCountLabel: UILabel!
+    
+   
+    @IBOutlet var cardButtons: [UIButton]!
+    
+    
+    @IBAction func touchCard(_ sender: UIButton) {
+        flipCount += 1
+        if let cardNumber = cardButtons.index(of: sender) {
+            flipCard(withEmoji: emojiChoices[cardNumber], on: sender)
+        } else {
+            print("index is nil")
+        }
+    }
+    
+     func flipCard(withEmoji emoji: String, on button: UIButton) {
+        if button.currentTitle == emoji {
+            button.setTitle("", for: UIControlState.normal)
             button.backgroundColor = #colorLiteral(red: 1, green: 0.5763723254, blue: 0, alpha: 1)
         }
         else {
-            button.setTitle("👻", for: UIControlState.normal)
+            button.setTitle(emoji, for: UIControlState.normal)
             button.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
         }
-        }
+    }
+    
 }
 
